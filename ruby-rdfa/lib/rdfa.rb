f376097039f83516472c9cf13a6a2827b09a45d4
benjaminlyu <benjaminlyu@b4e30e6d-962b-0410-a0ef-818d15195e0e>
2007-04-11 04:21:29 +0000

    added support for RDFa nested structures. In doing so, cleaned up how parser discovers the RDFa about between link/meta and other elements.

--- ruby-rdfa/lib/rdfa.rb
+++ ruby-rdfa/lib/rdfa.rb
@@ -44,6 +44,9 @@ module RdfA
       :bnode_name_generator => options[:bnode_name_generator])
   end
 
+
+  # ScreenCollector is an Ruby-RDFa collector that prints RDF
+  # statements to the screen
   class ScreenCollector
     attr_accessor :print_debug
 
@@ -84,6 +87,9 @@ module RdfA
     end
   end
 
+
+  # DictionaryCollector is a Ruby-RDFa that stores emitted RDFa statements
+  # into a dictionary object.
   class DictionaryCollector
     attr_accessor :base_uri
     attr_accessor :bnode_namespace
@@ -132,22 +138,39 @@ module RdfA
     end
   end
 
+
+  # CounterGenerator is a generic bnode name generator that uses
+  # an autoincrementing field to distinguish new nodes.
+  #
+  # NOTE: This Counter remembers the generated bnode URI for a given
+  #       rexml node.
   class CounterGenerator
     attr_accessor :namespace
     attr_accessor :prefix
 
     def initialize(options={})
-      @counter = 0
+      self.reset
       self.prefix = options[:prefix] ? options[:prefix] : DEFAULT_BNODE_PREFIX
       self.namespace = options[:namespace].nil? ? '' : options[:namespace]
     end
 
-    def generate
-      @counter += 1
-      "#{self.namespace}#{self.prefix}#{@counter}"
+    def generate(node)
+      if @bnode[node].nil?
+        @counter += 1
+        @bnode[node] = @counter
+      end
+      "#{self.namespace}#{self.prefix}#{@bnode[node]}"
+    end
+
+    def reset
+      @bnode = {}
+      @counter = 0
     end
   end
 
+
+  # RdfAParser is the workhorse RDFa parser.
+  #
   class RdfAParser
 
     def parse(document, options={})
@@ -201,84 +224,90 @@ module RdfA
           end unless index.nil?
         end
 
-        # Find about
-        about = current_node.attributes['about']
-        if about.nil? and current_node.name =~ /^(link|meta)$/
-          # We have a meta or link element, thus we only traverse
-          # to the single parent to search for an about.
-          # If the about doesn't exist, then check for the id
-          # attribute.
-          # Otherwise, create an anonymous name.
-          about = current_node.parent.attributes['about']
-          if about.nil? and current_node.parent.attributes['id']
-            about = "\##{current_node.parent.attributes['id']}"
-          end
-          # We need to check to see if the link's parent is the
-          # head element of an xhtml document.
-          if head and current_node.parent == head
-            # Get the document root element (xhtml) about.
-            about = document.root.attributes['about']
-            # No xhtml about found, the about is the document ''.
-            about = '' unless about
-          end
-          about = name_generator.generate if about.nil?
-        elsif about.nil?
-          tmp_node = current_node.parent
-          while about.nil? and !tmp_node.nil?
-            about = tmp_node.attributes['about']
-            tmp_node = tmp_node.parent
-          end
+        # Queue up the child elements for processing
+        current[:node].elements.each do |child|
+          queue << { :node => child, :ns => new_ns }
         end
-        about = '' if about.nil?
+
+        # Pull out the attributes needed for the skip test.
+        rel = current_node.attributes['rel']
+        rev = current_node.attributes['rev']
+        property = current_node.attributes['property']
+
+        # We skip to next node when there are zero RDFa predicates
+        next if(rel.nil? and rev.nil? and property.nil?)
+
+        # Pull out other important RDFa attributes
+        href = current_node.attributes['href']
+        content = current_node.attributes['content']
+
         begin
-          about = make_uri(new_ns, base_uri, about)
+          # Find about uri.
+          #
+          # We have special cases if the current node is a link or meta.
+          # In addition, we must check to see if a link or meta is
+          # within the head of an xhtml document (max_traversal of 1).
+          about_uri = nil
+          link_meta = current_node.name =~ /^(link|meta)$/
+          # if link_meta is not-nil, then max_traversal is 1; else nil.
+          max_traversal = (link_meta and 1) or nil
+          about_node = find_about_node(current_node, max_traversal)
+          # We now determine if the about_node is the head of an
+          # xhtml document. If so, then we have to handle this naming
+          # in a special way.
+          is_head = (link_meta and head and about_node == head) ? true : false
+          about_pre = about_uri_from_node(name_generator, about_node, is_head)
+          # Now normalize the about uri.
+          about_uri = make_uri(new_ns, base_uri, about_pre)
         rescue StandardError => e
           emit_warning(collector, current_node, e.to_s)
         end
 
-        # Get the href if it exists
-        href = nil
+        # Get the href
+        href_uri = nil
         begin
-          href = make_uri(new_ns, base_uri, current_node.attributes['href'])
+          # Make an href uri from the href attribute
+          href_uri = make_uri(new_ns, base_uri, href)
+          # We find the ID or anonymous bnode uri if we have a rel
+          # or rev in this node and no href.
+          if(href_uri.nil? and (rel or rev))
+            href_uri = bnode_uri_from_node(name_generator, current_node)
+          end
         rescue StandardError => e
           emit_warning(collector, current_node, e.to_s)
         end
 
         # find and emit the rel statement
         begin
-          rel = curie_to_uri(new_ns, current_node.attributes['rel'])
-          emit_triple(collector, about, rel, href) if about and rel and href
+          rel_uri = curie_to_uri(new_ns, rel)
+          emit_triple(collector, about_uri, rel_uri, href_uri) if rel_uri
         rescue StandardError => e
           emit_warning(collector, current_node, e.to_s)
         end
 
         # find and emit the rev statement
         begin
-          rev = curie_to_uri(new_ns, current_node.attributes['rev'])
-          emit_triple(collector, href, rev, about) if about and rev and href
+          rev_uri = curie_to_uri(new_ns, rev)
+          emit_triple(collector, href_uri, rev_uri, about_uri) if rev_uri
         rescue StandardError => e
           emit_warning(collector, current_node, e.to_s)
         end
 
         # find and emit the property statement
         begin
-          property  = curie_to_uri(new_ns, current_node.attributes['property'])
-          content = current_node.attributes['content']
+          property_uri  = curie_to_uri(new_ns, property)
+          # If no content attribute, then we just take the xml of the
+          # current node's children.
           if content.nil? and current_node.children.length > 0
             content = ''
             current_node.children.each do |child|
               content += child.to_s
             end
           end
-          emit_triple(collector, about, property, content) if(
-            about and property and content)
+          emit_triple(collector, about_uri, property_uri, content) if(
+            property_uri and content)
         rescue StandardError => e
           emit_warning(collector, current_node, e.to_s)
-        end
-
-        # Queue up the child elements for processing
-        current[:node].elements.each do |child|
-          queue << { :node => child, :ns => new_ns }
         end
       end
 
@@ -287,39 +316,9 @@ module RdfA
     end
 
     protected
-      def emit_base_uri(collector, base_uri)
-        if collector and collector.respond_to? :base_uri=
-          collector.base_uri = namespace
-        end
-      end
 
-      def emit_bnode_namespace(collector, namespace)
-        if collector and collector.respond_to? :bnode_namespace=
-          collector.bnode_namespace = namespace
-        end
-      end
-
-      def emit_namespace(collector, namespace)
-        if collector and collector.respond_to? :add_namespace
-          collector.add_namespace(namespace)
-        end
-      end
-
-      def emit_triple(collector, subject, predicate, object)
-        if collector and collector.respond_to? :add_triple
-          collector.add_triple(subject, predicate, object)
-        end
-      end
-
-      def emit_warning(collector, current_node, message)
-        if collector and collector.respond_to? :add_warning
-          collector.add_warning(message)
-        end
-        if collector and collector.respond_to? :add_debug
-          collector.add_debug(current_node.to_s, message)
-        end
-      end
-
+      # figures out the uri, which could be made from a CURIE or
+      # which may be normalized with a namespace and base uri.
       def make_uri(namespaces, base_uri, value)
         if value.nil?
           return nil
@@ -331,6 +330,84 @@ module RdfA
         end
       end
 
+      # Executes the RDFa about resolution process to find the node
+      # from which we can pull out an about uri.
+      #
+      # [startnode] -- the current node being processed
+      # [max_traversal] -- the number of ancestors to check, or nil to
+      #   check them all.
+      #
+      # returns nil if the about should reflect the document itself.
+      #
+      def find_about_node(startnode, max_traversal=nil)
+        # We traverse up the tree until one of the following:
+        # 1. We find the first about attribute of an ancestor
+        # 2. We find a rev or rel attribute in an element without an href.
+        # 3. We hit the document root.
+        current_node = startnode
+        count = 0
+        until current_node.nil?
+          about = current_node.attributes['about']
+          rel = current_node.attributes['rel']
+          rev = current_node.attributes['rev']
+          href = current_node.attributes['href']
+
+          # We're done if there's an about
+          return current_node if about
+
+          # Check to see if we have a rev or rel with nil href.
+          # But only for ancestors of the startnode
+          return current_node if(
+            current_node != startnode and (rel or rev) and href.nil?)
+
+          # We return if we hit max_traversal number of ancestors.
+          count += 1
+          return current_node if(max_traversal and count > max_traversal)
+
+          # We keep going
+          current_node = current_node.parent
+        end
+        # We went all the way way up to nil.
+        return nil
+      end
+
+      # Pulls out the about URI from the node that we care about.
+      #
+      # [node] -- The node returned by a call to find_about_node.
+      # [is_head] -- The node we are given is the head of an xhtml document.
+      #
+      def about_uri_from_node(name_generator, node, is_head)
+        # return '' because the node is nil, which means the about uri
+        # is the Document itself.
+        return '' if node.nil?
+        about = node.attributes['about']
+        # We have an about attribute, so we use that
+        return about if about
+        # Else, the node passed to us is of the following:
+        # 1. The link/meta's parent which may be head of xhtml document.
+        #    This implies: about=""
+        # 2. Some other parent node of a link or meta element.
+        # 3. An ancestor node that has a rel or rev while lacking
+        #    an href.
+        return '' if is_head
+        return bnode_uri_from_node(name_generator, node)
+      end
+
+      # We either return the bnode uri, or an ID URI fragment given
+      # a node.
+      #
+      # [name_generator] -- name generator
+      # [node] -- The current node from which to make the name.
+      #
+      # returns a URI string.
+      def bnode_uri_from_node(name_generator, node)
+        id = node.attributes['id']
+        return "\##{id}" if id
+        return name_generator.generate(node)
+      end
+
+      # Create a URI string based on a CURIE
+      #
       def curie_to_uri(namespaces, value)
         return nil unless value
 
@@ -346,6 +423,45 @@ module RdfA
           return URI.parse(uribase + split_value[1])
         else
           raise "invalid curie value: #{value}"
+        end
+      end
+
+
+      # helper function to emit an RDFa parsing event to the collector.
+      def emit_base_uri(collector, base_uri)
+        if collector and collector.respond_to? :base_uri=
+          collector.base_uri = namespace
+        end
+      end
+
+      # helper function to emit an RDFa parsing event to the collector.
+      def emit_bnode_namespace(collector, namespace)
+        if collector and collector.respond_to? :bnode_namespace=
+          collector.bnode_namespace = namespace
+        end
+      end
+
+      # helper function to emit an RDFa parsing event to the collector.
+      def emit_namespace(collector, namespace)
+        if collector and collector.respond_to? :add_namespace
+          collector.add_namespace(namespace)
+        end
+      end
+
+      # helper function to emit an RDFa parsing event to the collector.
+      def emit_triple(collector, subject, predicate, object)
+        if collector and collector.respond_to? :add_triple
+          collector.add_triple(subject, predicate, object)
+        end
+      end
+
+      # helper function to emit an RDFa parsing event to the collector.
+      def emit_warning(collector, current_node, message)
+        if collector and collector.respond_to? :add_warning
+          collector.add_warning(message)
+        end
+        if collector and collector.respond_to? :add_debug
+          collector.add_debug(current_node.to_s, message)
         end
       end
   end
